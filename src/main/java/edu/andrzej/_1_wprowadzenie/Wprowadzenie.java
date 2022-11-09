e8d4d14a5c74e11baa54a521dca0c150932f1ef0
atr <atrejman@wp.pl>
2022-11-09 16:49:43 +0100

    wprowadzenie

--- src/main/java/edu/andrzej/_1_wprowadzenie/Wprowadzenie.java
+++ src/main/java/edu/andrzej/_1_wprowadzenie/Wprowadzenie.java
@@ -1,4 +1,24 @@
 package edu.andrzej._1_wprowadzenie;
 
-public class Wprowadzenie \{
-        }
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Wprowadzenie {
+    public static void main(String[] args) {
+
+//        -----------------------
+
+//        Stream<Integer> s = Stream.iterate(1, x -> x + 1);
+//        s.forEach(System.out::println);
+
+//        -----------------------
+        List<String> imiona = Arrays.asList("Ala", "Jacek");
+        List<String> nowaListaImion = imiona
+                .stream()
+                .filter(imie -> imie.endsWith("a"))
+                .collect(Collectors.toList());
+        System.out.println(nowaListaImion);
+
+    }
+}
